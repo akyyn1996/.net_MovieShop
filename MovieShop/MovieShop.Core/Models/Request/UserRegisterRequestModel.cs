db539ef2dc22c30f2c83da2f00ebdcc9105d4e80
akyyn1996 <57305982+akyyn1996@users.noreply.github.com>
2020-12-01 05:07:33 +0800

    Register Part Finished

--- MovieShop/MovieShop.Core/Models/Request/UserRegisterRequestModel.cs
+++ MovieShop/MovieShop.Core/Models/Request/UserRegisterRequestModel.cs
@@ -1,7 +1,28 @@
-﻿namespace MovieShop.Core.Models.Request
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop.Core.Models.Request
 {
     public class UserRegisterRequestModel
     {
-        
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email  { get; set; }
+
+        [Required]
+        [StringLength(50,ErrorMessage = "Make Sure Password Is Right Length", MinimumLength = 8)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$", ErrorMessage = "Password Should have minimum 8 with at least one upper, lower, number and special character")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+
+        // create a custom class that derives from validation atrribute
     }
 }
---
+++ MovieShop/MovieShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieShop.Core.Models.Request;
+using MovieShop.Core.Models.Response;
+
+namespace MovieShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // http:localhost/account/register GET
+
+        [HttpGet]
+        // we need to show empty register page
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        // http:localhost/account/register POST
+        //when user hits submit button, post information to this method
+        [HttpPost]
+        //it also work with parameter not only model!!!!!!!!!!!!!!! not case sensitive!!!!!!!!!!!
+        public async Task<IActionResult> Register(UserRegisterRequestModel userRegisterRequestModel)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+
+        {
+
+            return View();
+        }
+
+        //public async Task<IActionResult> Login(LoginRequestModel loginRequest)
+
+        //{
+
+        //    return View();
+        //}
+    }
+}
